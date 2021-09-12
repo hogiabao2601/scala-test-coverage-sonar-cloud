faadf683ed3e420468f4695200f6c06fb880a6b2
Bao Ho <bao.ho@tookitaki.com>
2021-09-12 14:49:46 +0700

    Update sonar cloud 19

--- src/main/scala/bao/ho/ServiceAlg.scala
+++ src/main/scala/bao/ho/ServiceAlg.scala
@@ -3,6 +3,6 @@ package bao.ho
 trait ServiceAlg {
   def add(a: Int, b: Int): Int
   def minus(a: Int, b: Int): Int
-  def divide(a: Int, b: Int): Int
-  def multi(a: Int, b: Int): Int
+//  def divide(a: Int, b: Int): Int
+//  def multi(a: Int, b: Int): Int
 }
--- src/main/scala/bao/ho/ServiceImpl.scala
+++ src/main/scala/bao/ho/ServiceImpl.scala
@@ -5,61 +5,61 @@ class ServiceImpl extends ServiceAlg {
 
   override def minus(a: Int, b: Int): Int = a - b
 
-  override def divide(a: Int, b: Int): Int = {
-    if (b == 0 || a == 0) {
-      0
-    } else {
-      a / b
-    }
-  }
-
-  override def multi(a: Int, b: Int): Int = a * b
-
-  def run(a: Int, b: Int): Int = {
-    (add(a, _))
-      .andThen(minus(3, _))
-      .andThen(multi(3, _))
-      .andThen(minus(3, _))
-      .andThen(add(3, _))
-      .andThen(multi(3, _))
-      .andThen(divide(3, _))
-      .andThen(add(3, _))
-      .andThen(multi(3, _))
-      .andThen(minus(3, _))
-      .andThen(add(3, _))
-      .andThen(minus(3, _))
-      .andThen(minus(3, _))
-      .andThen(divide(3, _))
-      .andThen(add(3, _))
-      .andThen(multi(3, _))
-      .andThen(minus(3, _))
-      .andThen(add(3, _))
-      .andThen(minus(3, _))
-      .andThen(minus(3, _))
-      .apply(b)
-  }
-
-  def run2(a: Int, b: Int): Int = {
-    (add(a, _))
-      .andThen(minus(3, _))
-      .andThen(multi(3, _))
-      .andThen(minus(3, _))
-      .andThen(add(3, _))
-      .andThen(multi(3, _))
-      .andThen(divide(3, _))
-      .andThen(add(3, _))
-      .andThen(multi(3, _))
-      .andThen(minus(3, _))
-      .andThen(add(3, _))
-      .andThen(minus(3, _))
-      .andThen(minus(3, _))
-      .andThen(divide(3, _))
-      .andThen(add(3, _))
-      .andThen(multi(3, _))
-      .andThen(minus(3, _))
-      .andThen(add(3, _))
-      .andThen(minus(3, _))
-      .andThen(minus(3, _))
-      .apply(b)
-  }
+//  override def divide(a: Int, b: Int): Int = {
+//    if (b == 0 || a == 0) {
+//      0
+//    } else {
+//      a / b
+//    }
+//  }
+//
+//  override def multi(a: Int, b: Int): Int = a * b
+//
+//  def run(a: Int, b: Int): Int = {
+//    (add(a, _))
+//      .andThen(minus(3, _))
+//      .andThen(multi(3, _))
+//      .andThen(minus(3, _))
+//      .andThen(add(3, _))
+//      .andThen(multi(3, _))
+//      .andThen(divide(3, _))
+//      .andThen(add(3, _))
+//      .andThen(multi(3, _))
+//      .andThen(minus(3, _))
+//      .andThen(add(3, _))
+//      .andThen(minus(3, _))
+//      .andThen(minus(3, _))
+//      .andThen(divide(3, _))
+//      .andThen(add(3, _))
+//      .andThen(multi(3, _))
+//      .andThen(minus(3, _))
+//      .andThen(add(3, _))
+//      .andThen(minus(3, _))
+//      .andThen(minus(3, _))
+//      .apply(b)
+//  }
+//
+//  def run2(a: Int, b: Int): Int = {
+//    (add(a, _))
+//      .andThen(minus(3, _))
+//      .andThen(multi(3, _))
+//      .andThen(minus(3, _))
+//      .andThen(add(3, _))
+//      .andThen(multi(3, _))
+//      .andThen(divide(3, _))
+//      .andThen(add(3, _))
+//      .andThen(multi(3, _))
+//      .andThen(minus(3, _))
+//      .andThen(add(3, _))
+//      .andThen(minus(3, _))
+//      .andThen(minus(3, _))
+//      .andThen(divide(3, _))
+//      .andThen(add(3, _))
+//      .andThen(multi(3, _))
+//      .andThen(minus(3, _))
+//      .andThen(add(3, _))
+//      .andThen(minus(3, _))
+//      .andThen(minus(3, _))
+//      .apply(b)
+//  }
 }
