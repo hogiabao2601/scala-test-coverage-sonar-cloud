5c2e32c32cadcf41afea7ffcabff008a46e6f1b2
Bao Ho <bao.ho@tookitaki.com>
2021-09-12 13:46:29 +0700

    Update sonar cloud 10

--- src/main/scala/bao/ho/ServiceImpl.scala
+++ src/main/scala/bao/ho/ServiceImpl.scala
@@ -14,4 +14,52 @@ class ServiceImpl extends ServiceAlg {
   }
 
   override def multi(a: Int, b: Int): Int = a * b
+
+  def run(a: Int, b: Int): Int = {
+    (add(a, _))
+      .andThen(minus(3, _))
+      .andThen(multi(3, _))
+      .andThen(minus(3, _))
+      .andThen(add(3, _))
+      .andThen(multi(3, _))
+      .andThen(divide(3, _))
+      .andThen(add(3, _))
+      .andThen(multi(3, _))
+      .andThen(minus(3, _))
+      .andThen(add(3, _))
+      .andThen(minus(3, _))
+      .andThen(minus(3, _))
+      .andThen(divide(3, _))
+      .andThen(add(3, _))
+      .andThen(multi(3, _))
+      .andThen(minus(3, _))
+      .andThen(add(3, _))
+      .andThen(minus(3, _))
+      .andThen(minus(3, _))
+      .apply(b)
+  }
+
+  def run2(a: Int, b: Int): Int = {
+    (add(a, _))
+      .andThen(minus(3, _))
+      .andThen(multi(3, _))
+      .andThen(minus(3, _))
+      .andThen(add(3, _))
+      .andThen(multi(3, _))
+      .andThen(divide(3, _))
+      .andThen(add(3, _))
+      .andThen(multi(3, _))
+      .andThen(minus(3, _))
+      .andThen(add(3, _))
+      .andThen(minus(3, _))
+      .andThen(minus(3, _))
+      .andThen(divide(3, _))
+      .andThen(add(3, _))
+      .andThen(multi(3, _))
+      .andThen(minus(3, _))
+      .andThen(add(3, _))
+      .andThen(minus(3, _))
+      .andThen(minus(3, _))
+      .apply(b)
+  }
 }
