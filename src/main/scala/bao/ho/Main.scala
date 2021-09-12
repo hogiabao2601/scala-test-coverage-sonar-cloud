3552b785c5c43150c08eca89e6a5bf48f1e214f1
Bao Ho <bao.ho@tookitaki.com>
2021-09-12 14:57:01 +0700

    Update sonar cloud 20

--- src/main/scala/bao/ho/Main.scala
+++ src/main/scala/bao/ho/Main.scala
@@ -1,12 +1,12 @@
 package bao.ho
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    val service: ServiceAlg = new ServiceImpl
-    val s                   = service.add(1, 1)
-    println(s"s = $s")
-
-    val m = service.minus(2, 1)
-    println(s"m = $m")
-  }
+//  def main(args: Array[String]): Unit = {
+//    val service: ServiceAlg = new ServiceImpl
+//    val s                   = service.add(1, 1)
+//    println(s"s = $s")
+//
+//    val m = service.minus(2, 1)
+//    println(s"m = $m")
+//  }
 }
