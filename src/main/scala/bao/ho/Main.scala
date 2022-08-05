df9ba85ba072d1d9bbdd56aa9b68edea94e971c1
Bao Ho <9564463+hogiabao2601@users.noreply.github.com>
2022-08-05 14:47:01 +0800

    Update Main.scala

--- src/main/scala/bao/ho/Main.scala
+++ src/main/scala/bao/ho/Main.scala
@@ -1,12 +1,14 @@
 package bao.ho
 
 object Main {
-//  def main(args: Array[String]): Unit = {
+  val LOG = 1000;
+ def main(args: Array[String]): Unit = {
+   println(s"Hello Work")
 //    val service: ServiceAlg = new ServiceImpl
 //    val s                   = service.add(1, 1)
 //    println(s"s = $s")
 //
 //    val m = service.minus(2, 1)
 //    println(s"m = $m")
-//  }
+ }
 }
